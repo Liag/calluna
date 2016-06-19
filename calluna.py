ad95291016233727ca03cf80c18be15996d311c9
Liag <liag247@gmail.com>
2016-06-19 11:38:54 +0200

    Changed license to GPLv3.

--- calluna.py
+++ calluna.py
@@ -2,8 +2,22 @@
 # coding=utf-8
 #
 # Imports Rundata file data into Postgres DB
-# By Victor Rådmark
-# Updated: 2016-05-18
+# Copyright (C) 2016  Victor Rådmark
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Updated: 2016-06-18
 
 import json, argparse, configparser, pg8000, xlrd
 
