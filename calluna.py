3b35e1c587c9bffa81461dd14d459791a1b50503
R <liag247@gmail.com>
2019-05-06 02:59:37 +0200

    change name

--- calluna.py
+++ calluna.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 #
 # Imports Rundata file data into Postgres DB
-# Copyright (C) 2016  Victor Rådmark
+# Copyright (C) 2019  Runa Rådmark
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
